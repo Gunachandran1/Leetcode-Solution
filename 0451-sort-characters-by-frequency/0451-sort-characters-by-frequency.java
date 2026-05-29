2cc72a5d1627b2826c8bdd71053b604f7b7b8a55
Gunachandiran D <145830197+Gunachandran1@users.noreply.github.com>
2026-05-29 15:14:38 +0530

    Time: 113 ms (6.86%), Space: 46.5 MB (77.32%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,32 +1,29 @@
 class Solution {
     public String frequencySort(String s) {
-
-        StringBuilder sb = new StringBuilder();
         
+        StringBuilder sb = new StringBuilder();
         Map<Character,Integer> mpp = new HashMap<>();
 
         for(int i = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
             
-            char ele = s.charAt(i);
-            if(mpp.containsKey(ele)) mpp.put(ele,mpp.get(ele)+1);
-            else mpp.put(ele,1);
+            if(mpp.containsKey(ch)) mpp.put(ch,mpp.get(ch)+1);
+            else mpp.put(ch,1);
         }
 
-        PriorityQueue<Character> pq = new PriorityQueue<>((a,b) -> mpp.get(b) - mpp.get(a));
+        PriorityQueue<Character> pq = new PriorityQueue<>( (a,b) -> mpp.get(b) - mpp.get(a));
 
         for(char key : mpp.keySet()){
             pq.add(key);
         }
-        // System.out.println(pq);
-
-        // dont use for - each loop , instead 
-        // use the priority queue methods for best results
 
         while(pq.size() > 0){
 
-            char ele = pq.poll();
+            char ch = pq.poll();
 
-            for(int i = 0 ; i < mpp.get(ele) ; i++) sb.append(ele);
+            for(int i = 0 ; i < mpp.get(ch) ; i++){
+                sb.append(ch);
+            }
         }
 
         return sb.toString();
