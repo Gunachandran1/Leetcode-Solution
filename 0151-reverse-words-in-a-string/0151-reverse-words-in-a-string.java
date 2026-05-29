00d82293f8fb2351e9693970026d6965315440d0
Gunachandiran D <145830197+Gunachandran1@users.noreply.github.com>
2026-05-29 18:58:04 +0530

    Time: 8 ms (37.71%), Space: 46.8 MB (13.04%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -2,26 +2,22 @@ class Solution {
     public String reverseWords(String s) {
         
         int i = 0;
-        String ans = "";
         int n = s.length();
+        String ans = "";
 
         while(i < n){
-            
-            while(i < n && s.charAt(i)==' ') i++;
+            while(i < n && s.charAt(i) == ' ') i++;
             if(i >= n) break;
             int j = i+1;
+            while(j < n && s.charAt(j) != ' ') j++;
 
-            while(j < n && s.charAt(j)!=' ') j++;
+            String substr = s.substring(i,j);
 
-            String subStr = s.substring(i,j);
-
-            if(ans.length() == 0) ans += subStr;
-            else{
-                ans = subStr + " " + ans;
-            }
-            i = j+1;
+            if(ans.length() == 0) ans += substr;
+            else ans = substr + " " + ans;
+             i = j+1;
         }
 
-        return ans;
+         return ans;
     }
 }
