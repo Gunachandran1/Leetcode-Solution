f9ca3dc9b509f49797cc92f6cb048a88f3de6404
Gunachandiran D <145830197+Gunachandran1@users.noreply.github.com>
2026-05-30 12:53:12 +0530

    Time: 61 ms (8.12%), Space: 72.7 MB (10.97%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        
+        // maxi length subarray with atmost 2 types of numbers
+
+        int maxi = 0;
+
+        int l = 0 , r = 0 , n = fruits.length;
+        Map<Integer,Integer> mpp = new HashMap<>();
+
+        while(r < n){
+            if(!mpp.containsKey(fruits[r])) mpp.put(fruits[r],1);
+            else mpp.put(fruits[r],mpp.get(fruits[r])+1);
+
+            while(mpp.size() > 2){
+                mpp.put(fruits[l],mpp.get(fruits[l])-1);
+                if(mpp.get(fruits[l]) == 0) mpp.remove(fruits[l]);
+                l++;
+            }
+            int len = r - l + 1;
+            if(len > maxi) maxi = len;
+            r++;
+        }
+
+        return maxi;
+    }
+}
