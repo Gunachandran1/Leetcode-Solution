be2c15f9a592a8cc0c8f98f79405bd0338607ecf
Gunachandiran D <145830197+Gunachandran1@users.noreply.github.com>
2026-06-02 11:44:39 +0530

    Time: 23 ms (93.8%), Space: 48.8 MB (60.32%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -11,9 +11,9 @@ class Solution {
         for(int i = 0 ; i  < arr.length ; i++){
 
             preSum += arr[i];
-            int remove = preSum - k;
-            if(mpp.containsKey(remove)){
-            count += (int)mpp.get(remove);
+            
+            if(mpp.containsKey(preSum - k)){
+            count += mpp.get(preSum - k);
             }
             if(mpp.containsKey(preSum)){
                 mpp.put(preSum,mpp.get(preSum)+1);
