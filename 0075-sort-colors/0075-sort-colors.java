2bdf90e3d9cf2e2390de2876e0de03644a17dc46
Gunachandiran D <145830197+Gunachandran1@users.noreply.github.com>
2026-06-02 14:14:53 +0530

    Time: 0 ms (100%), Space: 43.3 MB (75.09%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,32 +1,33 @@
 class Solution {
     public void sortColors(int[] nums) {
-
-        // DUTCH NATIONAL FLAG ALGORITHM
-
+        
+        int  n = nums.length;
+        
         int low = 0;
         int mid = 0;
-        int high = nums.length-1;
+        int high = n-1;
 
         while(mid <= high){
             if(nums[mid] == 0){
-                swap(mid,low,nums);
-                low++;
-                mid++;
+                swap(low , mid , nums);
+                low ++;
+                mid ++;
             }
-            else if(nums[mid] == 1){
-                mid++;
-            }
+            else if(nums[mid] == 1) mid++;
             else{
-                swap(mid,high,nums);
+                swap(mid , high , nums);
                 high--;
             }
         }
-
     }
 
     void swap(int a , int b , int[] arr){
-        int temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
+        while(a<b){
+            int temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+            a++;
+            b--;        
+        }
     }
 }
