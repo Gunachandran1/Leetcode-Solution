ac22bbaba1a719ee54a6c084b1e065e2ae6ff591
Gunachandiran D <145830197+Gunachandran1@users.noreply.github.com>
2026-06-02 14:09:56 +0530

    Time: 45 ms (23.51%), Space: 46.9 MB (82.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,32 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        HashMap<Integer,Integer> mpp = new HashMap<>();
-        HashMap<Integer,Integer> mpp2 = new HashMap<>();
-        
-        for(int i = 0 ; i < nums.length ; i++){
-            if(!mpp.containsKey(nums[i])){
-                mpp.put(nums[i],i);
-            }
-            else{
-            mpp2.put(nums[i],i);
-            }
-        }
-        Arrays.sort(nums);
 
-        int i = 0 ; int j = nums.length-1;
-
-        while(i < j){
-            if(nums[i]+nums[j] == target){
-                if(nums[i] == nums[j]){
-                    
-                    return new int[]{mpp.get(nums[i]) , mpp2.get(nums[i])};
-                }
-                return new int[]{mpp.get(nums[i]),mpp.get(nums[j])};
+        for(int i = 0 ; i < nums.length -1 ; i++){
+            for(int j = i+1 ; j < nums.length ; j++){
+                if(nums[i]+nums[j] == target)return new int[]{i,j};
             }
-            if(nums[i]+nums[j] > target) j--;
-            else i++;
-        }
-    return new int[]{-1,-1};
+        } 
+        return new int[]{-1,-1};
     }
 }
