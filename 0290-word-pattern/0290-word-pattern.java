a333697bfc4c1840d3bbad21cee26bc8b3de7aad
Gunachandiran D <145830197+Gunachandran1@users.noreply.github.com>
2026-06-03 14:32:21 +0530

    Time: 1 ms (81.91%), Space: 42.6 MB (71.62%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,26 +1,24 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
+        
+        String[] strArr = s.split(" ");
+        if(strArr.length != pattern.length()) return false;
 
-        String[] wordArr = s.split(" ");
-
-        if(wordArr.length != pattern.length()) return false;
-
-        HashMap<Character,String> mpp1 = new HashMap<>();
-        HashMap<String,Character> mpp2 = new HashMap<>();
+        HashMap<Character,String> hmp1 = new HashMap<>();
+        HashMap<String,Character> hmp2 = new HashMap<>();
 
         for(int i = 0 ; i < pattern.length() ; i++){
-
             char ele = pattern.charAt(i);
-            String word = wordArr[i];
 
-            if(mpp1.containsKey(ele) && (!mpp1.get(ele).equals(word))
-            || mpp2.containsKey(word) && (!mpp2.get(word).equals(ele)))
-            return false;
+            if(hmp1.containsKey(ele) && (!hmp1.get(ele).equals(strArr[i])) ||
+                hmp2.containsKey(strArr[i]) && (!hmp2.get(strArr[i]).equals(ele)))
+                return false;
 
-            mpp1.put(ele,word);
-            mpp2.put(word,ele);
+            hmp1.put(ele,strArr[i]);
+            hmp2.put(strArr[i],ele);
+
         }
-        return true;
 
+        return true;
     }
 }
