0a097829a40fa23677056f89d4d03bb0cc3d4982
Gunachandiran D <145830197+Gunachandran1@users.noreply.github.com>
2026-06-04 16:49:19 +0530

    Time: 1 ms (100%), Space: 50.7 MB (85.46%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,15 +1,24 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
+        return calCount(nums,goal) - calCount(nums,goal-1);
         
+    }
+
+    public int calCount(int[] nums , int goal){
+
+        if(goal < 0) return 0;
+
         int count = 0;
-        
+        int l = 0;
         int n = nums.length;
-        for(int l = 0 ; l < n ; l++){
-            int sum = 0;
-            for(int r = l ; r < n ; r++){
-                sum += nums[r];
-                if(sum == goal) count++;
+        int sum = 0;
+        for(int r = 0 ; r < n ; r++){
+            sum += nums[r];
+            while(sum > goal){
+                sum -= nums[l];
+                l++;
             }
+            count += (r-l+1);
         }
         return count;
     }
