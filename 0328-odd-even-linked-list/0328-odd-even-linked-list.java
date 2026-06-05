cc103e70bab4d47d503b24f852770a11e19a422e
Gunachandiran D <145830197+Gunachandran1@users.noreply.github.com>
2026-06-05 19:26:49 +0530

    Time: 0 ms (100%), Space: 46.2 MB (87.13%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -10,40 +10,71 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
+
+            if( head == null || head.next == null) return head;
+
+            ListNode odd = head;
+            ListNode even = head.next;
+            ListNode evenHead = even;
+
+            while(odd.next != null && even.next != null){
+                
+                odd.next = odd.next.next;
+                odd = odd.next;
+                even.next = even.next.next;
+                even = even.next;
+            }
+
+            odd.next = evenHead;
+
+            return head;
+
+
+
+
+
+
+
+
+
+
+
+
+
         
-        if(head == null) return null;
-        List<Integer> lst = new ArrayList<>();
+        // if(head == null) return null;
+        // List<Integer> lst = new ArrayList<>();
 
-        ListNode temp = head;
+        // ListNode temp = head;
         
-        while(temp != null && temp.next != null){
-            lst.add(temp.val);
-            temp = temp.next.next;
-        }
-        if(temp != null){
-            lst.add(temp.val);
-        }
-        System.out.println(lst);
+        // while(temp != null && temp.next != null){
+        //     lst.add(temp.val);
+        //     temp = temp.next.next;
+        // }
+        // if(temp != null){
+        //     lst.add(temp.val);
+        // }
+        // System.out.println(lst);
         
 
-        temp = head.next;
+        // temp = head.next;
 
-        while(temp != null && temp.next != null){
-            lst.add(temp.val);
-            temp = temp.next.next;
-        }
-        if(temp != null){
-            lst.add(temp.val);
-        }
+        // while(temp != null && temp.next != null){
+        //     lst.add(temp.val);
+        //     temp = temp.next.next;
+        // }
+        // if(temp != null){
+        //     lst.add(temp.val);
+        // }
 
-        temp = head;
-        int i = 0;
-        while(temp != null){
-            temp.val = lst.get(i++);
-            temp = temp.next;
-        }
+        // temp = head;
+        // int i = 0;
+        // while(temp != null){
+        //     temp.val = lst.get(i++);
+        //     temp = temp.next;
+        // }
 
-        return head;
+        // return head;
 
     }
 }
