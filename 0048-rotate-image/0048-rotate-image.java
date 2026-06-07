3e771a25bc1e73e1aa5b3716a69d4096a2df9473
Gunachandiran D <145830197+Gunachandran1@users.noreply.github.com>
2026-06-08 00:46:56 +0530

    Time: 0 ms (100%), Space: 43.8 MB (50.73%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,26 +1,33 @@
 class Solution {
-    public void rotate(int[][] mat) {
+    public void rotate(int[][] matrix) {
         
-        for(int i = 0 ; i < mat.length - 1; i++){
-            for(int j = i+1 ; j < mat[i].length ; j++){
-                int temp = mat[j][i];
-                mat[j][i] = mat[i][j];
-                mat[i][j] = temp;
+        for(int i = 0 ; i <= matrix.length-2 ; i++){
+            for(int j = i+1 ; j < matrix[i].length ;j++){
+                swap(i ,j , matrix);
             }
         }
 
-        for(int[] arr : mat){
-            int i = 0 ;
-            int  j = arr.length - 1;
-            while(i < j){
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                i++;
-                j--;
-            }
-            
+        for(int[] arr : matrix){
+            rev(arr);
         }
     }
-    
+
+    public void swap(int a , int b , int[][] matrix){
+        int temp = matrix[a][b];
+        matrix[a][b] = matrix[b][a];
+        matrix[b][a] = temp;
+    }
+
+    public void rev(int[] arr)
+    {
+        int st = 0;
+        int end = arr.length-1;
+        while(st < end){
+            int temp = arr[st];
+            arr[st] = arr[end];
+            arr[end] = temp;
+            st++;
+            end--;
+        }
+    }
 }
