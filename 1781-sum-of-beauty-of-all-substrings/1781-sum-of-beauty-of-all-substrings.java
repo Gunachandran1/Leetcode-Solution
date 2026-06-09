aa165b9d225047bcd603311888d308a1b4438a86
Gunachandiran D <145830197+Gunachandran1@users.noreply.github.com>
2026-06-09 11:52:48 +0530

    Time: 63 ms (47.34%), Space: 43.9 MB (82.26%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -1,33 +1,30 @@
 class Solution {
-    
-        
-
-
     public int beautySum(String s) {
-        if(s==null || s.length() <= 1) return 0;
+        
         int sum = 0;
-        int n = s.length();
 
-        for(int i = 0 ; i < n ; i++){
-            int[] freq = new int[26];
-            int maxi = 0;
-            for(int j = i ; j < n ; j++){
-                int index = s.charAt(j)-'a';
-                freq[index]++;
-                maxi = Math.max(maxi,freq[index]);
-
-                int mini = Integer.MAX_VALUE;
-
-            for(int ele : freq){
-                if(ele > 0) 
-                mini = Math.min(mini,ele);
-            }
+        for(int i = 0 ; i < s.length() ; i++){
+            
+            int[] hash = new int[26];
+            int max = 0;
+            for(int j = i ; j < s.length() ; j++){
                 
-                sum += (maxi - mini);
+                hash[s.charAt(j)-'a']++;
+
+                max = Math.max(max,hash[s.charAt(j)-'a']);
+
+                int min = Integer.MAX_VALUE;
+                
+
+                for(int ele : hash){
+                    if(ele != 0 && ele < min) min = ele;
+                }
+                if(min != Integer.MAX_VALUE)
+                sum += (max-min);
+                
             }
         }
 
-
-        return sum ;
+        return sum;
     }
 }
