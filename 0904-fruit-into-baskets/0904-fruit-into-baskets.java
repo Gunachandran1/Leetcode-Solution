849800c537dcc2118418c33b3555b51fedddbca6
Gunachandiran D <145830197+Gunachandran1@users.noreply.github.com>
2026-06-15 11:50:19 +0530

    Time: 63 ms (5.21%), Space: 72.9 MB (8.89%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -3,16 +3,17 @@ class Solution {
         
         int i = 0;
         int j = 0;
-        int maxi = 0;
+        int  maxi = 0;
 
+        int n = fruits.length;
         HashMap<Integer,Integer> mpp = new HashMap<>();
 
-        while(j < fruits.length){
+        while(j < n){
 
             if(!mpp.containsKey(fruits[j])) mpp.put(fruits[j],1);
-            else mpp.put(fruits[j] , mpp.get(fruits[j])+1);
+            else mpp.put(fruits[j],mpp.get(fruits[j])+1);
 
-            if(mpp.size() > 2){
+            while(mpp.size() > 2){
                 mpp.put(fruits[i],mpp.get(fruits[i])-1);
                 if(mpp.get(fruits[i]) == 0) mpp.remove(fruits[i]);
                 i++;
@@ -20,6 +21,8 @@ class Solution {
 
             int len = j - i + 1;
             if(len > maxi) maxi = len;
+            
+
 
             j++;
         }
