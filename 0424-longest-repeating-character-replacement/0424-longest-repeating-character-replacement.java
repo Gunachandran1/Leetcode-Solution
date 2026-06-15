3265b6dfaabfdd2f7c25e30ff648904002a7ae81
Gunachandiran D <145830197+Gunachandran1@users.noreply.github.com>
2026-06-15 14:23:55 +0530

    Time: 44 ms (8.96%), Space: 47.4 MB (7.05%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -18,7 +18,7 @@ class Solution {
             maxFreq = Math.max(maxFreq,mpp.get(ch));
 
 
-            while(((j - i + 1) - maxFreq) > k){
+            if(((j - i + 1) - maxFreq) > k){
                 mpp.put(s.charAt(i),mpp.get(s.charAt(i))-1);
                 if(mpp.get(s.charAt(i)) == 0) mpp.remove(s.charAt(i));
                 i++;
