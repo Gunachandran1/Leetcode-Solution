833e665ea7fac5767e778ba74d70d044d6293a95
Gunachandiran D <145830197+Gunachandran1@users.noreply.github.com>
2026-08-06 19:27:45 +0530

    Time: 0 ms (100%), Space: 61.4 MB (29.96%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,24 +1,31 @@
 class Solution {
+
+    void swap(int a , int b , int[] arr){
+
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    void reverse(int a , int b , int[] arr){
+        
+        while(a < b){
+            swap(a,b,arr);
+            a++;
+            b--;
+        }
+
+    }
+
     public void rotate(int[] nums, int k) {
 
         int n = nums.length;
         k = k%n;
-
-        if(n==1) return;
-        rev(0,n-k-1,nums);
-        rev(n-k , n-1 , nums);
-        rev(0 , n-1 , nums);
+        reverse(0,n-k-1,nums);
+        reverse(n-k,n-1,nums);
+        reverse(0,n-1,nums);
 
     }
 
-    void rev(int a , int b , int[] arr){
-        while(a < b)
-        {
-            int temp = arr[a];
-            arr[a] = arr[b];
-            arr[b] = temp;
-            a++;
-            b--;
-        }
-    }
+    
 }
