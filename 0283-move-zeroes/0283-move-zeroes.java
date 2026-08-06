45af205805ca207c363bc3ae9a1de54e17762a04
Gunachandiran D <145830197+Gunachandran1@users.noreply.github.com>
2026-08-06 19:33:52 +0530

    Time: 2 ms (92.13%), Space: 47.9 MB (38.88%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,27 +1,38 @@
 class Solution {
+
+    void swap(int a , int b , int[] arr){
+
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    int findFirstZero(int[] arr){
+
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i] == 0) return i;
+        }
+        return -1;
+    }
+
     public void moveZeroes(int[] nums) {
-        
+
         int n = nums.length;
-        if(n==1) return;
-        int zero = -1;
-        for(int i = 0 ; i < n ; i++){
-            if(nums[i] == 0){
-                zero = i;
-                break;
-            }
-        }
-
-        if(zero == -1) return;
-
-        int j = zero+1;
-        for(j = j ; j < n ; j++){
-            if(nums[j] != 0){
-                nums[zero] = nums[j];
-                nums[j] = 0;
-                zero++;
-            }
-        }
+        if(n == 1) return;
         
+        int firstZero = findFirstZero(nums);
+
+        if(firstZero == -1) return;
+
+        int j = firstZero+1;
+
+        while(j < n){
+            if(nums[j] != 0){
+                swap(firstZero , j , nums);
+                firstZero++;
+            }
+            j++;
+        }
 
     }
 }
