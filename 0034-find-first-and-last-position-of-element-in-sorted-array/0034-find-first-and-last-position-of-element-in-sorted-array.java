3213c111f0730f23c612f8a13b7edc261177b9f5
Gunachandiran D <145830197+Gunachandran1@users.noreply.github.com>
2026-08-23 17:16:19 +0530

    Time: 0 ms (100%), Space: 47.8 MB (94.54%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,42 +1,56 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         
-        int low = 0 , high = nums.length-1;
-        int[] arr = new int[2];
+        int[] res = {-1,-1};
 
-        while(low <= high){
+        int low = 0;
+        int high = nums.length-1;
 
-            int mid = low + ((high-low)/2);
-
-            if(nums[mid] >= target){
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target)
+            {
+                res[0] = mid;
                 high = mid - 1;
             }
-            else{
+            else if(nums[mid] > target)
+            {
+                high = mid - 1;
+            }
+            else 
+            {
                 low = mid + 1;
             }
         }
 
-        if((low == nums.length) || (nums[low]!=target)){
-            return new int[]{-1,-1};
-        }
-        arr[0] = low;
+        if(res[0] == -1) return res;
 
-        low = 0 ; high = nums.length-1;
+        low = 0;
+        high = nums.length-1;
 
-        while(low <= high){
-
-            int mid = low + ((high - low )/2);
-
-            if(nums[mid] > target){
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target)
+            {
+                res[1] = mid;
+                low = mid + 1;
+            }
+            else if(nums[mid] > target)
+            {
                 high = mid - 1;
             }
-            else{
+            else 
+            {
                 low = mid + 1;
             }
         }
 
-        arr[1] = low-1;
+        return res;
 
-        return arr;
+
+
+
     }
 }
