96ad02c741c6b14b0477654089f023ec4f941555
Gunachandiran D <145830197+Gunachandran1@users.noreply.github.com>
2026-08-26 17:23:00 +0530

    Time: 1 ms (58.49%), Space: 49 MB (9.12%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int trap(int[] height) {
+        
+        if(height.length <= 2) return 0;
+
+        int[] lbArray = new int[height.length];
+        lbArray[0] = height[0];
+        int[] rbArray = new int[height.length];
+        rbArray[height.length-1] = height[height.length-1];
+
+
+        for(int i = 1 ; i < height.length-1 ; i++)
+        {
+            if(height[i] > lbArray[i-1])
+            {
+                lbArray[i] = height[i];
+            }
+            else
+            {
+                lbArray[i] = lbArray[i-1];
+            }
+        }
+
+        lbArray[height.length-1] = lbArray[height.length-2];
+
+        for(int i = height.length-2 ; i > 0 ; i--)
+        {
+            if(height[i] > rbArray[i+1])
+            {
+                rbArray[i] = height[i];
+            }
+            else
+            {
+                rbArray[i] = rbArray[i+1];
+            }
+        }
+
+        rbArray[0] = rbArray[1];
+
+
+        // System.out.print(Arrays.toString(lbArray));
+        // System.out.print(Arrays.toString(rbArray));
+
+        long total = 0;
+
+        for(int i = 1 ; i < height.length-1 ; i++)
+        {
+            int waterLevel = Math.min(lbArray[i],rbArray[i]);
+
+            total += waterLevel - height[i];
+        }
+        
+        return (int)total;
+    }
+}
