d1f1ebcd3b6229c4fa52c73001134930ac5732c9
Gunachandiran D <145830197+Gunachandran1@users.noreply.github.com>
2026-08-26 18:00:24 +0530

    Time: 0 ms (100%), Space: 47.6 MB (81.22%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -3,53 +3,86 @@ class Solution {
         
         if(height.length <= 2) return 0;
 
-        int[] lbArray = new int[height.length];
-        lbArray[0] = height[0];
-        int[] rbArray = new int[height.length];
-        rbArray[height.length-1] = height[height.length-1];
+        // -----------------------------------------------------------------------
+
+        // int[] lbArray = new int[height.length];
+        // lbArray[0] = height[0];
+        // int[] rbArray = new int[height.length];
+        // rbArray[height.length-1] = height[height.length-1];
 
 
-        for(int i = 1 ; i < height.length-1 ; i++)
-        {
-            if(height[i] > lbArray[i-1])
-            {
-                lbArray[i] = height[i];
-            }
-            else
-            {
-                lbArray[i] = lbArray[i-1];
-            }
-        }
+        // for(int i = 1 ; i < height.length-1 ; i++)
+        // {
+        //     if(height[i] > lbArray[i-1])
+        //     {
+        //         lbArray[i] = height[i];
+        //     }
+        //     else
+        //     {
+        //         lbArray[i] = lbArray[i-1];
+        //     }
+        // }
 
-        // lbArray[height.length-1] = lbArray[height.length-2];   we dont checking last and first 
+        // // lbArray[height.length-1] = lbArray[height.length-2];   we dont checking last and first 
 
-        for(int i = height.length-2 ; i >= 0 ; i--)
-        {
-            if(height[i] > rbArray[i+1])
-            {
-                rbArray[i] = height[i];
-            }
-            else
-            {
-                rbArray[i] = rbArray[i+1];
-            }
-        }
+        // for(int i = height.length-2 ; i >= 0 ; i--)
+        // {
+        //     if(height[i] > rbArray[i+1])
+        //     {
+        //         rbArray[i] = height[i];
+        //     }
+        //     else
+        //     {
+        //         rbArray[i] = rbArray[i+1];
+        //     }
+        // }
 
-        // rbArray[0] = rbArray[1];    we dont check last and first (see below i , its starting from i and moving all the way till 2nd last element)
+        // // rbArray[0] = rbArray[1];    we dont check last and first (see below i , its starting from i and moving all the way till 2nd last element)
 
 
-        // System.out.print(Arrays.toString(lbArray));
-        // System.out.print(Arrays.toString(rbArray));
+        // // System.out.print(Arrays.toString(lbArray));
+        // // System.out.print(Arrays.toString(rbArray));
 
-        long total = 0;
+        // long total = 0;
 
-        for(int i = 1 ; i < height.length-1 ; i++)
-        {
-            int waterLevel = Math.min(lbArray[i],rbArray[i]);
+        // for(int i = 1 ; i < height.length-1 ; i++)
+        // {
+        //     int waterLevel = Math.min(lbArray[i],rbArray[i]);
 
-            total += waterLevel - height[i];
-        }
+        //     total += waterLevel - height[i];
+        // }
         
-        return (int)total;
+        // return (int)total;
+
+
+        // ---------------------------------------------------
+
+        int l = 0;
+        int r = height.length-1;
+
+        int lhb = height[l];
+        int rhb = height[r];
+
+        long res = 0;
+
+        while(l <= r)
+        {
+            if(lhb <= rhb)
+        {
+            if(height[l] >= lhb) lhb = height[l];
+            else res += (lhb - height[l]);
+
+            l++;
+        }
+        else
+        {
+            if(height[r] >= rhb) rhb = height[r];
+            else res += (rhb - height[r]);
+
+            r--;
+        }
+        }
+
+        return (int)res;
     }
 }
