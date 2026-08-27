8df2b83bd56badc071b4e2ffa30816f7577e9ad6
Gunachandiran D <145830197+Gunachandran1@users.noreply.github.com>
2026-08-27 21:13:31 +0530

    Time: 1 ms (25.73%), Space: 47 MB (11.59%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.java
+++ 0867-transpose-matrix/0867-transpose-matrix.java
@@ -1,6 +1,11 @@
 class Solution {
     public int[][] transpose(int[][] matrix) {
 
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        if(row != col)
+        {
         int[][] res = new int[matrix[0].length][matrix.length];
         
         for(int i = 0 ; i < matrix.length ; i++)
@@ -11,7 +16,22 @@ class Solution {
             }   
         }
 
-
         return res;
+        }
+        for(int i = 0 ; i < matrix.length ; i++)
+        {
+            for(int j = i+1 ; j < matrix[0].length ; j++)
+            {   
+                int temp = matrix[i][j];    
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }   
+        }
+
+        return matrix;
+        
+        
     }
+
+    
 }
